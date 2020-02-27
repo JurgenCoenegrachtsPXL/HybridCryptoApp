4b5043d09ea696730820c5305995bbba631adffd
seymaciftciPXL <seyma.ciftci@student.pxl.be>
2020-02-27 17:16:41 +0100

    Deel 1

---
+++ HybridCryptoApp/HybridCryptoApp/Crypto/Random.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HybridCryptoApp.Crypto
+{
+    public class Random
+    {
+        public static byte[] GetNumbers(int length)
+        {
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                byte[] array = new byte[length];
+
+                random.GetBytes(array);
+
+                MessageBox.Show(array.ToString());
+
+                return array;
+            }
+        }
+    }
+}
