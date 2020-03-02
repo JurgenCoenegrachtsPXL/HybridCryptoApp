221a628b4d3d06a55dc47f911fd9c4bcc9258fa4
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-03-02 08:16:48 +0100

    Hashing testen voor argon2 geschreven

--- HybridCryptoApp/HybridCryptoApp.Tests/Crypto/HashingTests.cs
+++ HybridCryptoApp/HybridCryptoApp.Tests/Crypto/HashingTests.cs
@@ -6,11 +6,21 @@ namespace HybridCryptoApp.Tests.Crypto
     [TestFixture]
     public class HashingTests
     {
+        private byte[] salt;
+        private byte[] key;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            salt = Random.GetNumbers(32);
+            key = Random.GetNumbers(16);
+        }
+
         [Test]
         public void Hashing_Can_Generate_Hash_Of_Message()
         {
             string message = "Hello World";
-            byte[] hash = Hashing.Hash(message);
+            byte[] hash = Hashing.Argon2(message, salt, key);
 
             Assert.NotNull(hash);
             CollectionAssert.IsNotEmpty(hash);
@@ -20,8 +30,8 @@ namespace HybridCryptoApp.Tests.Crypto
         public void Hashing_Can_Generate_Same_Hash_For_Same_Message()
         {
             string message = "Hello World";
-            byte[] hash1 = Hashing.Hash(message);
-            byte[] hash2 = Hashing.Hash(message);
+            byte[] hash1 = Hashing.Argon2(message, salt, key);
+            byte[] hash2 = Hashing.Argon2(message, salt, key);
 
             CollectionAssert.AreEqual(hash1, hash2);
         }
@@ -32,8 +42,40 @@ namespace HybridCryptoApp.Tests.Crypto
             string message1 = "Hello World";
             string message2 = "Goodbye World";
 
-            byte[] hash1 = Hashing.Hash(message1);
-            byte[] hash2 = Hashing.Hash(message2);
+            byte[] hash1 = Hashing.Argon2(message1, salt, key);
+            byte[] hash2 = Hashing.Argon2(message2, salt, key);
+
+            CollectionAssert.AreNotEqual(hash1, hash2);
+        }
+
+        [Test]
+        public void Hashing_Can_Generate_Hash_Of_Data()
+        {
+            byte[] data = Random.GetNumbers(256);
+            byte[] hash = Hashing.Argon2(data, salt, key);
+
+            Assert.NotNull(hash);
+            CollectionAssert.IsNotEmpty(hash);
+        }
+
+        [Test]
+        public void Hashing_Can_Generate_Same_Hash_For_Same_Data()
+        {
+            byte[] data = Random.GetNumbers(256);
+            byte[] hash1 = Hashing.Argon2(data, salt, key);
+            byte[] hash2 = Hashing.Argon2(data, salt, key);
+
+            CollectionAssert.AreEqual(hash1, hash2);
+        }
+
+        [Test]
+        public void Hashing_Generates_Different_Hash_For_Different_Data()
+        {
+            byte[] data1 = Random.GetNumbers(256);
+            byte[] data2 = Random.GetNumbers(256);
+
+            byte[] hash1 = Hashing.Argon2(data1, salt, key);
+            byte[] hash2 = Hashing.Argon2(data2, salt, key);
 
             CollectionAssert.AreNotEqual(hash1, hash2);
         }
