8547b83937f8993a81ad6d6741942979dcad6178
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-03-02 21:27:17 +0100

    HybridEncryption klasse voorbereid

---
+++ HybridCryptoApp/HybridCryptoApp/Crypto/HybridEncryption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridCryptoApp.Crypto
+{
+    public class HybridEncryption
+    {
+        public static EncryptedPacket Encrypt(byte[] data)
+        {
+            // TODO: create EAS session key
+            // TODO: create EAS IV
+            // TODO: encrypt data with EAS
+            // TODO: generate hash of encrypted data
+            // TODO: encrypt EAS session key with RSA
+            // TODO: put into new packet
+            throw new NotImplementedException();
+        }
+
+        public static byte[] Decrypt(EncryptedPacket encryptedPacket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
