7b95838a105ffb25f58f94021ab47965b556a08e
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-03-02 08:30:09 +0100

    symmetric encryption testen geschreven

--- HybridCryptoApp/HybridCryptoApp.Tests/Crypto/SymmetricEncryptionTests.cs
+++ HybridCryptoApp/HybridCryptoApp.Tests/Crypto/SymmetricEncryptionTests.cs
@@ -1,10 +1,69 @@
-﻿using NUnit.Framework;
+﻿using HybridCryptoApp.Crypto;
+using NUnit.Framework;
 
 namespace HybridCryptoApp.Tests.Crypto
 {
     [TestFixture]
     public class SymmetricEncryptionTests
     {
+        private byte[] key;
+        private byte[] iv;
 
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            key = Random.GetNumbers(32);
+            iv = Random.GetNumbers(32);
+        }
+
+        [Test]
+        public void Can_Encrypt_Data()
+        {
+            byte[] rawData = Random.GetNumbers(256);
+
+            byte[] encryptedData = SymmetricEncryption.Encrypt(rawData, key, iv);
+            Assert.NotNull(encryptedData);
+            CollectionAssert.IsNotEmpty(encryptedData);
+        }
+
+        [Test]
+        public void Encrypted_Data_Differs_From_Raw_data()
+        {
+            byte[] rawData = Random.GetNumbers(256);
+
+            byte[] encryptedData = SymmetricEncryption.Encrypt(rawData, key, iv);
+            CollectionAssert.AreNotEqual(rawData, encryptedData);
+        }
+
+        [Test]
+        public void Can_DecryptData()
+        {
+            byte[] rawData = Random.GetNumbers(256);
+
+            byte[] decryptedData = SymmetricEncryption.Decrypt(rawData, key, iv);
+            Assert.NotNull(decryptedData);
+            CollectionAssert.IsNotEmpty(decryptedData);
+        }
+
+        [Test]
+        public void Decrypted_Data_Differs_From_Raw_data()
+        {
+            byte[] rawData = Random.GetNumbers(256);
+
+            byte[] decryptedData = SymmetricEncryption.Encrypt(rawData, key, iv);
+            CollectionAssert.AreNotEqual(rawData, decryptedData);
+        }
+
+        [Test]
+        public void Encrypted_Data_Can_Be_Decrypted()
+        {
+            byte[] rawData = Random.GetNumbers(256);
+
+            byte[] encryptedData = SymmetricEncryption.Encrypt(rawData, key, iv);
+
+            byte[] decryptedData = SymmetricEncryption.Decrypt(encryptedData, key, iv);
+
+            CollectionAssert.AreEqual(rawData, decryptedData);
+        }
     }
 }
