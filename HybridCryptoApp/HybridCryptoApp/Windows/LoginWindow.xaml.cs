3a3ebe61e752796560e229be0be15a011612f6ae
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-04-14 18:39:57 +0200

    Adds support for loading and sharing public RSA key

--- HybridCryptoApp/HybridCryptoApp/Windows/LoginWindow.xaml.cs
+++ HybridCryptoApp/HybridCryptoApp/Windows/LoginWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using HybridCryptoApp.Crypto;
 using HybridCryptoApp.Networking;
 
 namespace HybridCryptoApp.Windows
@@ -40,9 +31,17 @@ namespace HybridCryptoApp.Windows
 
             try
             {
-                await Client.Login(EmailTextBox.Text, PasswordPasswordBox.Password);
+                // load RSA key
+                AsymmetricEncryption.SelectKeyPair(RSAKeyTextBox.Text, 4096);
 
-                await Task.Delay(1_000);
+                // log in and wait for at least 1.5s, whichever finishes last
+                List<Task> tasks = new List<Task>();
+
+                tasks.Add(Client.Login(EmailTextBox.Text, PasswordPasswordBox.Password));
+
+                tasks.Add(Task.Delay(1_500));
+
+                await Task.WhenAll(tasks);
 
                 //move to chat window
                 ChatWindow chat = new ChatWindow();
